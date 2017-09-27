c0722e9fa19651b51f825a9afaecaa7e0ff72d1d
maciej-ka <maciej.kasprzyk.it@gmail.com>
2017-09-27 20:29:33 +0200

    motors

--- book/03-motors/06-brushless/project.ino
+++ book/03-motors/06-brushless/project.ino
@@ -1,20 +1,24 @@
 #include <Servo.h>
 
 Servo myservo;
-int pos = 0;
+int pos = 90;
 
 void setup() {
   myservo.attach(9);
-  myservo.write(pos);
-  delay(1000);
+
+  // set range
+  myservo.write(180);
+  delay(2000);
+  myservo.write(90);
+  delay(8000);
 }
 
 void loop() {
-  for(pos = 0; pos < 180; pos += 1) {
+  for(pos = 90; pos < 180; pos += 1) {
     myservo.write(pos);
     delay(15);
   }
-  for(pos = 180; pos >= 1; pos -= 1) {
+  for(pos = 180; pos >= 90; pos -= 1) {
     myservo.write(pos);
     delay(15);
   }
